685fb3120db86bc0b151f4caf657d7aa18bd2468
Kusma Singh <kusma.insonix@gmail.com>
2017-07-20 11:29:26 +0530

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule,Routes} from '@angular/router';
+import {Component} from '@angular/core';
+
+//Service Imports
+import {UserService} from './user.service'
+
+//Guard Import
+import { AuthguardGuard } from './authguard.guard'; 
+
+const appRoutes:Routes=[
+  {
+path:'',
+component: LoginFormComponent
+},
+
+{
+path:'dashboard',
+canActivate:[AuthguardGuard],   
+component: DashboardComponent
+}
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginFormComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
-  providers: [],
+  providers: [UserService, AuthguardGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/authguard.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+//service import
+import { UserService } from './user.service';
+
+@Injectable()
+export class AuthguardGuard implements CanActivate {
+  constructor(private user: UserService) {
+
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.user.getUserLoggedIn();
+  }
+} 
